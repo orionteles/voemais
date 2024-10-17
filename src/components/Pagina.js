7100600cac79574b69fdab52a4f6b29af18a5b3d
Orion Teles de Mesquita <orionteles@gmail.com>
2024-10-17 19:32:04 -0300

    Menu

--- src/components/Pagina.js
+++ src/components/Pagina.js
@@ -7,41 +7,17 @@ export default function Pagina(props) {
                 <Container>
                     <Navbar.Brand href="/">Fundamentos</Navbar.Brand>
                     <Nav className="me-auto">
-                        <NavDropdown title="Filmes" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="/filmes/categoria/popular">
-                               Populares
+                        <NavDropdown title="Backend" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="/backend/empresas">
+                               Empresas
                             </NavDropdown.Item>
                             <NavDropdown.Divider />
-                            <NavDropdown.Item href="/filmes/categoria/now_playing">
-                               Em cartaz
-                            </NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="/filmes/categoria/upcoming">
-                                Em Breve
-                            </NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="/filmes/categoria/top_rated">
-                                Bem avaliados
-                            </NavDropdown.Item>
                         </NavDropdown>
-                        <NavDropdown title="Séries" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="/series/categoria/popular">
-                               Populares
-                            </NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="/series/categoria/on_the_air">
-                               No ar
-                            </NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="/series/categoria/airing_today">
-                                Estreando
-                            </NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="/series/categoria/top_rated">
-                                Bem avaliados
-                            </NavDropdown.Item>
-                        </NavDropdown>
-                        <Nav.Link href="/atores">Atores</Nav.Link>
+                        <Nav.Link href="/empresas">Empresas</Nav.Link>
+                        <Nav.Link href="/aeroportos">Aeroportos</Nav.Link>
+                        <Nav.Link href="/voos">Voos</Nav.Link>
+                        <Nav.Link href="/passagens">Passagens</Nav.Link>
+                        <Nav.Link href="/passageiros">Passageiros</Nav.Link>
                     </Nav>
                 </Container>
             </Navbar>
