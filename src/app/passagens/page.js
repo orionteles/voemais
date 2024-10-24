ee548374856111cbdc7b63e248107a808300e1bb
Orion Teles de Mesquita <orionteles@gmail.com>
2024-10-24 20:31:24 -0300

    Passagens

--- src/app/passagens/page.js
+++ src/app/passagens/page.js
@@ -10,15 +10,20 @@ export default function Page() {
     const fileiras = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     const colunas = ['A', 'B', 'C', 'D', 'E', 'F']
 
-    const bloqueadas = ['1A', '1B', '1C', '2A', '2B', '2C', '3C', '4C', '9F', '4F']
+    const [selecionado, setSelecionado] = useState(['5A', '5B', '5C'])
+
+    const bloqueadas = [
+        '1A', '1B', '1C', 
+        '2A', '2B', '2C', 
+        '3C', '4C', '9F', 
+        '4F'
+    ]
 
     return (
 
         <>
-            <h1>Assentos</h1>
-
-            <div className="container" style={{ width: 500, background: '#f1f1f1' }}>
-
+            <div className="container mt-3" style={{ width: 500, background: '#f1f1f1' }}>
+                <p>Assento Selecionado: {selecionado}</p>
                 <Row>
                     {colunas.map(coluna => (
                         <>
@@ -35,8 +40,12 @@ export default function Page() {
                                 {coluna == 'D' && <Col />}
                                 <Col>
                                     {bloqueadas.includes(fila+coluna) ?
-                                    <MdChair style={{ fontSize: 40 }} /> :
-                                    <MdOutlineChair style={{ fontSize: 40 }} />
+                                    <MdChair style={{ fontSize: 40, color: '#ccc'}}/> :
+                                    (
+                                        selecionado.includes(fila+coluna) ?
+                                        <MdChair style={{ fontSize: 40, color: 'green' }}  /> : 
+                                        <MdOutlineChair onClick={()=>setSelecionado(fila+coluna)} style={{ fontSize: 40, color: '#000' }}  />
+                                    )
                                     }
                                 </Col>
                             </>
